f93b919b7801ec418fdb63f56c70e83ad0829941
ahn_mac <licors214@gmail.com>
2021-09-13 23:56:50 +0900

    add MemberService and test code

---
+++ spring-basic/src/main/java/com/example/springbasic/service/MemberService.java
@@ -0,0 +1,44 @@
+package com.example.springbasic.service;
+
+import com.example.springbasic.domain.Member;
+import com.example.springbasic.repository.MemberRepository;
+import com.example.springbasic.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+// service 에서는 비지니스 로직에 가까운 용어 선택
+// 서비스는 주로 비지니스 로직 처리
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        // cmd + opt + v 로 자동변수 선언 가능
+        // ctrl + t  refactory 여러 기능 호출
+        // optional 에서 orElseGet() 이 많이 쓰임(있으면 리턴 없으면 뒤에 메소드 실행)
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                // isPresent() 와 혼동 주의
+               .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ spring-basic/src/test/java/com/example/springbasic/service/MemberServiceTest.java
@@ -0,0 +1,82 @@
+package com.example.springbasic.service;
+
+import com.example.springbasic.domain.Member;
+import com.example.springbasic.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    // 아래 경우에는 MemoryMemberRepository 가 테스트와 실제 동작 시 객체가 다를 수 있다.
+    // DI 를 통해 해결 가능
+//    MemberService memberService = new MemberService();
+//    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    // 예외 사항 테스트
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("hello");
+
+        Member member2 = new Member();
+        member2.setName("hello");
+
+        //when
+        // 아래 try catch 를 간략하게 표현하면 68라인과 같다.
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        //then
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
