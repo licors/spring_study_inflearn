f4362a45f4adcd3ca43659170aba3d3a6985e0a4
ahn_mac <licors214@gmail.com>
2021-09-17 12:39:17 +0900

    h2 DB 설치 및 스프링 연동하여 db 저장 JdbcTemplate 로 구현

--- spring-basic/src/main/java/com/example/springbasic/SpringConfig.java
+++ spring-basic/src/main/java/com/example/springbasic/SpringConfig.java
@@ -1,9 +1,13 @@
 package com.example.springbasic;
 
+import com.example.springbasic.repository.JdbcTemplateMemberRepository;
 import com.example.springbasic.repository.MemberRepository;
 import com.example.springbasic.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /*
     @Component 기타
@@ -15,10 +19,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     // 구현 시 어떤 DB 를 붙일지 못정한 경우
     // 이런 식으로 config 파일에 등록시켜 놓는다면 나중에 변경 시 편하다.
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
